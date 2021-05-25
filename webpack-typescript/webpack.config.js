7d78a8b99e7a5a491e8f34b2b0312f492422123e
heroyooi <heroyooi1018@gmail.com>
2021-05-26 08:38:19 +0900

    webpack-typescript 공부 완료

--- webpack-typescript/webpack.config.js
+++ webpack-typescript/webpack.config.js
@@ -1,9 +1,8 @@
 const path = require("path");
 
-const isDev = process.env.NODE_ENV !== "production";
-
 module.exports = {
-  mode: isDev ? "development" : "production",
+  mode: "development",
+  devtool: 'eval-source-map',
   entry: "./src/index.ts",
   module: {
     rules: [
